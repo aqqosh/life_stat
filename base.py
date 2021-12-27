3ad1b7d6c099374fad17b38602f441325aa69473
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-12-27 11:04:07 +0300

    plotly helloworld

---
+++ base.py
@@ -0,0 +1,98 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+df = pd.DataFrame({
+    "Task": ["Data preprocesing", "Leetcode", "Kaggle", "Leetcode"],
+    "Time": [180, 60, 150, 60],
+    "Category": ["Work", "Study", "Study", "Study"],
+    "Date": ["26/12/2021", "26/12/2021", "27/12/2021", "27/12/2021"]
+})
+
+fig = px.bar(df, x="Task", y="Time", color="Category", barmode="group")
+
+app.layout = html.Div([
+    
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+    
+    html.Div(children=[
+        html.Label('Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': u'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value='MTL'
+        ),
+
+        html.Br(),
+        html.Label('Multi-Select Dropdown'),
+        dcc.Dropdown(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': u'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value=['MTL', 'SF'],
+            multi=True
+        ),
+
+        html.Br(),
+        html.Label('Radio Items'),
+        dcc.RadioItems(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': u'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value='MTL'
+        ),
+    ], style={'padding': 10, 'flex': 1}),
+
+    html.Div(children=[
+        html.Label('Checkboxes'),
+        dcc.Checklist(
+            options=[
+                {'label': 'New York City', 'value': 'NYC'},
+                {'label': u'Montréal', 'value': 'MTL'},
+                {'label': 'San Francisco', 'value': 'SF'}
+            ],
+            value=['MTL', 'SF']
+        ),
+
+        html.Br(),
+        html.Label('Text Input'),
+        dcc.Input(value='MTL', type='text'),
+
+        html.Br(),
+        html.Label('Slider'),
+        dcc.Slider(
+            min=0,
+            max=9,
+            marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+            value=5,
+        ),
+    ], style={'padding': 10, 'flex': 1})
+], style={'display': 'flex', 'flex-direction': 'column'
+         }
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
